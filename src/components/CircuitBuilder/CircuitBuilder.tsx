3a5953e9268eb98af38612144f8a0e6d1d9db2c3
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-05 10:57:44 +1100

    updated circuit builder component

--- src/components/CircuitBuilder/CircuitBuilder.tsx
+++ src/components/CircuitBuilder/CircuitBuilder.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import CircuitArrangement from './CircuitArrangement';
+import styles from './CircuitBuilder.module.scss';
+import Toolbar from './Toolbar';
 const CircuitBuilder : React.FC = () => {
-    return (<div>
-        This is circuit Builder
+    return (
+        <div className={styles.circuitBuilder}>
+        <Toolbar/>
+        <CircuitArrangement/>
+        <div className={styles.utilities}>
+            <button>Compress circuit</button>
+            <button>Zoom in/Zoom out</button>
+        </div>
+
     </div>)
 }
 
