b16974ffbcd10e4b6aa9e80a5be383868c7f2a91
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-05 10:58:31 +1100

    updated header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import styles from './Header.module.scss';
 import logo from '../../assets/logo.svg'
 
 const Header : React.VFC  = () => {
-
+    // const location = useLocation();
 
     return (<div className={styles.header}>
         <div className={styles.logo}>
             <img alt="Quokka" src={logo}/>
             <h1>Quokka</h1>
         </div>
-        {/*<Link to="" className={styles.logo}>*/}
-        {/*    <img alt="Quokka" src={logo}/>*/}
-        {/*</Link>*/}
 
 
+        <div className={styles.menu}>
+            <div className={styles.status} >
+                <p>Quokka Status: </p>
+                <p>Not connected</p>
+            </div>
+
+            <div className={styles.loginMenu}>
+                <p>Login/Create Account</p>
+            </div>
+
+
+            <div className={styles.navButtons}>
+                <NavLink to="/about" className={(navData) => navData.isActive ? styles.selected : ""}>About</NavLink>
+                <NavLink to="/setup" className={(navData) => navData.isActive ? styles.selected : ""}>Setup Quokka</NavLink>
+                <NavLink to="/circuit-builder" className={(navData) => navData.isActive ? styles.selected : ""}>Circuit Builder</NavLink>
+                <NavLink to="/circuit-output" className={(navData) => navData.isActive ? styles.selected : ""}>Circuit Output</NavLink>
+            </div>
+
+
+        </div>
     </div>)
 }
 
