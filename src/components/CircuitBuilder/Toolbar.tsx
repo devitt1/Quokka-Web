b5c9d39b31315c63964c1a0ff49d7d182f66e935
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-05 11:00:38 +1100

    Added preliminary implementation of toolbar, circuitArrangement, circuit builder styling, refactored stylesheet (name)

---
+++ src/components/CircuitBuilder/Toolbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './CircuitBuilder.module.scss';
+
+const Toolbar : React.FC = () => {
+    return (<div className={styles.toolbar}>
+        <h3>New Untitled Cicuit</h3>
+    </div>)
+}
+
+export default Toolbar;
---
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,10 @@
+
+const PageNotFound : React.VFC  = () => {
+
+    return (<div>
+        Page Not Found
+    </div>)
+}
+
+
+export default PageNotFound;
