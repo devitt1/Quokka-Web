b2e12c0e6c5dda3010a02ec16745cfb01c537b3b
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-10 13:38:47 +1100

    updated setup page

--- src/components/Setup/Setup.tsx
+++ src/components/Setup/Setup.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import styles from './Setup.module.scss';
 
 const Setup : React.FC = () => {
-    return (<div>
-        This is setup
+    return (<div className={styles.setup}>
+        <p>Instructions/informational page for how to connect the mobile to Quokka & also Quokka to the circuit builder</p>
+        <p>Cater for standard text and images. This page needs to work on all devices.</p>
     </div>)
 }
 
