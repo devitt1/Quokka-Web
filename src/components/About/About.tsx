bc01e3acb36fbd6c564aaaee1b2cb6255b499549
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-10 13:53:32 +1100

    refactored About page and style

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import styles from './About.module.scss';
 const About : React.FC = () => {
-    return (<div>
-        This is About
+    return (<div className={styles.about}>
+        <p>Marketing information about what Quokka is and where to get it.</p>
+        <p>Cater for standard text and images. This page needs to work on all devices.</p>
     </div>)
 }
 
