a91cd62f0a085bdbd013a9549be8ec4dacb9eb37
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-12 16:54:33 +1100

    update to use redux tools in browser

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -2,6 +2,14 @@ import {applyMiddleware, createStore, compose} from "@reduxjs/toolkit";
 import rootReducer from '../reducers/rootReducer';
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
