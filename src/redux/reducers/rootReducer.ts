d9b1ffc6c07f7f310a22e226e4265719eb01450e
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-12 16:56:44 +1100

    updated root reducer

--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,8 +1,14 @@
 import {combineReducers} from "@reduxjs/toolkit";
+import {deviceConnectionReducer, DeviceConnectionState } from "./deviceConnectionReducer";
+import {modalsReducer, ModalsState } from "./modalsReducer";
 
 export interface RootState {
+    modals : ModalsState
+    deviceConnection : DeviceConnectionState
 }
 const rootReducer = combineReducers<RootState>({
+    modals : modalsReducer,
+    deviceConnection : deviceConnectionReducer
 })
 
 export default rootReducer;
