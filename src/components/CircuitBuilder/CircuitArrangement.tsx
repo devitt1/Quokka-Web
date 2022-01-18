a3d6c2d6641861c4b5056a748482e33c3e75b367
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-18 18:05:18 +1100

    added references to newly implemented grid

--- src/components/CircuitBuilder/CircuitArrangement.tsx
+++ src/components/CircuitBuilder/CircuitArrangement.tsx
@@ -1,29 +1,15 @@
 import React, { useState } from 'react';
 import styles from './CircuitBuilder.module.scss';
-import { SVGArea } from './SVGArea';
-import { SVGGrid } from './SVGGrid';
-import { Qubit } from './Qubit';
-
+import Grid from "./Grid";
 interface CircuitArrangementProps {
     qubits : {id : string, number : number }[],
-    draggedData : any
 }
 
 const CircuitArrangement : React.FC<CircuitArrangementProps> = (props) => {
 
 
     return (<div className={styles.circuitArrangement}>
-            {/*<SVGArea draggedData={props.draggedData}/>*/}
-            {/*<ul>*/}
-            {/*    {props.qubits.map((qubit) => {*/}
-            {/*            return (*/}
-            {/*                <li><Qubit/></li>*/}
-            {/*            );*/}
-            {/*        }*/}
-            {/*    )}*/}
-            {/*</ul>*/}
-            <SVGGrid/>
-
+        <Grid></Grid>
 
         </div>
     )
