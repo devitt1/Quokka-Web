5874f5e9e9b2847bb1e1aaa118cd4240e3a0327a
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-18 18:02:37 +1100

    updated circuit output component, added circuit output item

--- src/components/CircuitOutput/CircuitOutput.tsx
+++ src/components/CircuitOutput/CircuitOutput.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import styles from './CircuitOutput.module.scss';
+import CircuitOutputItem from "./CircuitOutputItem";
 
-const CicuitOutput : React.FC = () => {
-    return (<div>
-        This is circuit ouptut
+const CircuitOutput : React.FC = () => {
+
+    var empty = false;
+
+    const CircuitOutputItemArray = [
+
+    ]
+
+    return (<div className={styles.circuitOutput}>
+        <h2>Circuit Output</h2>
+
+
+        {
+            !empty ?
+                <CircuitOutputItem/>
+                :
+                <div className={styles.prompt}>
+                    <p>Run a circuit in the Circuit Builder and visit this page again once complete to see the output.</p>
+                    <p className={styles.warningMessage}>Circuit output will only be accessible until cookies are cleared. Login or create an account to save circuit output.</p>
+                </div>
+        }
+
+
     </div>)
 }
 
-export default CicuitOutput;
+export default CircuitOutput;
