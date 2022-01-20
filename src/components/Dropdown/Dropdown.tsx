8a0bd8073b63f45bbddbf3c175683cdc600b5c0d
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-20 17:26:39 +1100

    updated dropdown components

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Dropdown.module.scss';
 import arrow_down_black from '../../assets/arrow_down_black.svg';
 
 interface DropdownProps {
     name : string;
     borderStyle: string;
+    dropdownContent: string[];
 }
 
 
 export const Dropdown : React.FC<DropdownProps> = (props) => {
     var dropDownBtnStyle = styles.dropdown;
+    const [toggleDropdown, setToggleDropdown] = useState(false);
     if (props.borderStyle === "roundedRightCorner"){
         dropDownBtnStyle = styles.roundedRightCorner;
+
+    }
+
+    if (toggleDropdown) {
+        dropDownBtnStyle += ' ' + styles.selected;
+    }
+
+    const handleDropdownClicked = () => {
+        console.log("dropdown clicked");
+        setToggleDropdown(!toggleDropdown);
+
     }
 
     return <div className={styles.dropdown}>
-        <button className={dropDownBtnStyle}>
+        <button className={dropDownBtnStyle} onClick={handleDropdownClicked}>
             {props.name}
             <img src={arrow_down_black}/>
         </button>
+
+        {toggleDropdown
+        ?
+            <div className={styles.dropdownList}>
+                {
+                    props.dropdownContent.map((dropdownItem) =>
+                        (<div className={styles.dropdownItem}>
+                            <p>{dropdownItem}</p>
+                        </div>)
+                    )
+                }
+            </div>
+            :
+            null
+
+        }
+
 
     </div>
 }
