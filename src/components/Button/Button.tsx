f0cda36e2546a7fbd9e4b1023967c1d3ce167174
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-20 17:27:13 +1100

    updated button components, interactivity

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,18 +4,23 @@ import styles from './Button.module.scss';
 interface ButtonProps {
     name : string;
     buttonStyle : string;
+    onClick : any;
+    selected : boolean
 }
 
 export const Button : React.FC<ButtonProps> = (props) => {
     var buttonStyle = styles.button;
     if (props.buttonStyle === "selectGateBtn") {
-        buttonStyle = styles.button + ' ' + styles.selectGateBtn;
+        buttonStyle += ' ' + styles.selectGateBtn;
+        if (props.selected) {
+            buttonStyle += ' ' + styles.selected;
+        }
     } else if (props.buttonStyle === "runCircuitBtn") {
         buttonStyle = styles.button + ' ' + styles.runCircuitBtn;
     }
 
     return (<div className={styles.button}>
-        <button className={buttonStyle}>
+        <button className={buttonStyle} onClick={props.onClick}>
             {props.name}
         </button>
 
