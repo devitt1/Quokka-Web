96434feb8bbbfba3133cb21d7928a74f88cd03de
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-21 15:47:56 +1100

    updated dropdown standard gate with dispatch

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import styles from './Dropdown.module.scss';
 import arrow_down_black from '../../assets/arrow_down_black.svg';
-
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../redux/reducers/rootReducer";
+import {updateSelectedStandardGate} from "../../redux/actions/circuitConfigAction";
 interface DropdownProps {
     name : string;
     borderStyle: string;
@@ -10,11 +12,12 @@ interface DropdownProps {
 
 
 export const Dropdown : React.FC<DropdownProps> = (props) => {
+    const dispatch = useDispatch();
+
     var dropDownBtnStyle = styles.dropdown;
     const [toggleDropdown, setToggleDropdown] = useState(false);
     if (props.borderStyle === "roundedRightCorner"){
         dropDownBtnStyle = styles.roundedRightCorner;
-
     }
 
     if (toggleDropdown) {
@@ -25,6 +28,12 @@ export const Dropdown : React.FC<DropdownProps> = (props) => {
         console.log("dropdown clicked");
         setToggleDropdown(!toggleDropdown);
 
+    }
+
+    const handleDropdownItemClicked = (dropdownItem : string) => {
+        console.log(`dropdown item clicked ${dropdownItem}`);
+
+        dispatch(updateSelectedStandardGate(dropdownItem));
     }
 
     return <div className={styles.dropdown}>
@@ -38,7 +47,7 @@ export const Dropdown : React.FC<DropdownProps> = (props) => {
             <div className={styles.dropdownList}>
                 {
                     props.dropdownContent.map((dropdownItem) =>
-                        (<div className={styles.dropdownItem}>
+                        (<div className={styles.dropdownItem} onClick={() => handleDropdownItemClicked(dropdownItem)}>
                             <p>{dropdownItem}</p>
                         </div>)
                     )
