3cfc5f3b31827bafdb96d13e8c6b631ff4e12365
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-24 11:42:16 +1100

    updated change from svg to g tags to avoid scaling issue

--- src/components/CircuitBuilder/GridRow.tsx
+++ src/components/CircuitBuilder/GridRow.tsx
@@ -6,27 +6,25 @@ import {useSelector} from "react-redux";
 import {RootState} from "../../redux/reducers/rootReducer";
 
 interface GridRowProps {
-    rowVerticalOffset : number
+    rowVerticalOffset : number;
+    gridCols : number[];
 }
 
 const GridRow : React.FC<GridRowProps> = (props) => {
     const { cursor, setCursor } = useContext(CursorContext);
     const {selectedStandardGate} = useSelector((state: RootState) => state.circuitConfig);
 
-    const slots = Array.from(Array(34).keys());
-
-    return (<svg className={styles.gridRow} y="39" width="100%" height="100%" >
+    return (<g className={styles.gridRow} y={props.rowVerticalOffset} width="100%" height="100%" >
             <line className={styles.line} x1="0" y1={props.rowVerticalOffset + 20} x2="100%"
             y2={props.rowVerticalOffset + 20}/>
             {
-                slots.map((number) => {
+                props.gridCols.map((number) => {
                     const xPos = number * 48;
-                    return (<GridCell cellXPos={xPos} cellYPos={props.rowVerticalOffset} hasGate={false}
-                   gate={'X'}/>)
+                    return (<GridCell cellXPos={xPos} cellYPos={props.rowVerticalOffset} hasGate={false}/>)
 
                 })
             }
-        </svg>
+        </g>
     )
 
 }
