e7f74971d77b729082a00e24a1074612580ba4fe
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-24 15:40:26 +1100

    added RowIndex for GridRow and updated props for GridCell components generation

--- src/components/CircuitBuilder/GridRow.tsx
+++ src/components/CircuitBuilder/GridRow.tsx
@@ -1,26 +1,25 @@
-import React, {useCallback, useContext} from 'react';
+import React from 'react';
 import styles from './CircuitBuilder.module.scss';
-import {CursorContext} from "../Providers/CursorContextProvider";
 import GridCell from "./GridCell";
-import {useSelector} from "react-redux";
-import {RootState} from "../../redux/reducers/rootReducer";
 
 interface GridRowProps {
     rowVerticalOffset : number;
     gridCols : number[];
+    rowIndex : number;
 }
 
-const GridRow : React.FC<GridRowProps> = (props) => {
-    const { cursor, setCursor } = useContext(CursorContext);
-    const {selectedStandardGate} = useSelector((state: RootState) => state.circuitConfig);
+const GridRow : React.FC<GridRowProps> = (children) => {
 
-    return (<g className={styles.gridRow} y={props.rowVerticalOffset} width="100%" height="100%" >
-            <line className={styles.line} x1="0" y1={props.rowVerticalOffset + 20} x2="100%"
-            y2={props.rowVerticalOffset + 20}/>
+    const {rowVerticalOffset, gridCols, rowIndex} = children;
+
+
+    return (<g className={styles.gridRow} y={rowVerticalOffset} width="100%" height="100%" >
+            <line className={styles.line} x1="0" y1={rowVerticalOffset + 20} x2="100%"
+            y2={rowVerticalOffset + 20}/>
             {
-                props.gridCols.map((number) => {
+                gridCols.map((number) => {
                     const xPos = number * 48;
-                    return (<GridCell cellXPos={xPos} cellYPos={props.rowVerticalOffset} hasGate={false}/>)
+                    return (<GridCell rowIndex={rowIndex} colIndex={number} cellXPos={xPos} cellYPos={rowVerticalOffset} hasGate={false}/>)
 
                 })
             }
