cd8f95427ab0ad6ffbb6a6de4777e5febe7e00b9
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-27 16:41:06 +1100

    removed prop

--- src/components/CircuitBuilder/CircuitArrangement.tsx
+++ src/components/CircuitBuilder/CircuitArrangement.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import styles from './CircuitBuilder.module.scss';
 import Grid from "./Grid";
 interface CircuitArrangementProps {
-    qubits : {id : string, number : number }[],
 }
 
 const CircuitArrangement : React.FC<CircuitArrangementProps> = (props) => {
