eb3edf53ac516c539e99f5fe8c39ccfe768e6f62
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-27 16:36:03 +1100

    added rowIndex, colIndex and gate id

--- src/common/classes.ts
+++ src/common/classes.ts
@@ -1,6 +1,7 @@
 import { DeviceConnectionState } from "../redux/reducers/deviceConnectionReducer";
 import {IGate} from "./interfaces";
 import {GateType} from "./types";
+import {v4} from 'uuid';
 
 export class DeviceConnection implements DeviceConnectionState {
     connected: boolean;
@@ -12,21 +13,29 @@ export class DeviceConnection implements DeviceConnectionState {
 }
 
 export class Gate implements IGate {
+    id : string;
     x: number;
     y: number;
     width: number;
     height: number;
+    rowIndex: number;
+    colIndex : number;
     type: GateType;
     constructor(
         x: number,
         y: number,
         width: number,
         height : number,
+        rowIndex : number,
+        colIndex : number,
         type: GateType) {
+        this.id = v4();
        this.x = x;
        this.y = y;
        this.width = width;
        this.height = height;
+       this.rowIndex = rowIndex;
+       this.colIndex = colIndex;
        this.type = type;
     }
 }
--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,9 +1,12 @@
 import {GateType} from "./types";
 
 export interface IGate {
+    id : string,
     x : number,
     y : number,
     width : number,
     height : number,
+    rowIndex : number,
+    colIndex : number,
     type: GateType
 }
