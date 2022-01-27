2b635add43daca26c700824ae1c76f120c33426d
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-27 16:45:03 +1100

    removed prop

--- src/components/CircuitBuilder/CircuitBuilder.tsx
+++ src/components/CircuitBuilder/CircuitBuilder.tsx
@@ -2,30 +2,15 @@ import React, {useState, useEffect} from 'react';
 import CircuitArrangement from './CircuitArrangement';
 import styles from './CircuitBuilder.module.scss';
 import Toolbar from './Toolbar';
-import CursorContextProvider from "../Providers/CursorContextProvider";
-import Cursor from "../Cursor/Cursor";
-
-const initialQubits = [
-    {id : "00", number : 1},
-    {id : "01", number : 2},
-    {id : "02", number : 3},
-];
 
 const CircuitBuilder : React.FC = () => {
-    const [qubits, setQubits] = useState(initialQubits);
-
-
-
-    useEffect(() => {
-        console.log(qubits);
-    }, [qubits])
 
 
     return (
             <div className={styles.circuitBuilder}>
                 <Toolbar/>
 
-                <CircuitArrangement qubits={qubits}>
+                <CircuitArrangement>
                 </CircuitArrangement>
                 <div className={styles.utilities}>
                     <button >Compress circuit</button>
