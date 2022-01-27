c89a9e5b8bb8ea8573e951cf82dc37dd90f3e648
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-27 16:36:57 +1100

    added circuit config actions add remove dropped gate, dragging gate

--- src/redux/actions/circuitConfigAction.ts
+++ src/redux/actions/circuitConfigAction.ts
@@ -5,6 +5,12 @@ export const UPDATE_SELECTED_STANDARD_GATE_ACTION = "UPDATE_SELECTED_STANDARD_GA
 export const UPDATE_DEFAULT_STANDARD_GATE_ACTION = "UPDATE_DEFAULT_STANDARD_GATE";
 export const UPDATE_GATE_SELECT_MODE_ACTION = "UPDATE_GATE_SELECT_MODE";
 export const ADD_DROPPED_GATE_ACTION = "ADD_DROPPED_GATE";
+export const REMOVE_DROPPED_GATE_ACTION = "REMOVE_DROPPED_GATE";
+export const UPDATE_DRAGGING_GATE_ACTION = "UPDATE_DRAGGING_GATE";
+export const REMOVE_DRAGGING_GATE_ACTION = "REMOVE_DRAGGING_GATE";
+export const UPDATE_DRAGGING_GATE_POSITION = "UPDATE_DRAGGING_GATE_POSITION"
+
+
 export const updateSelectedStandardGate = (gate : string) => async (dispatch : any) =>
 {
     try {
@@ -36,12 +42,69 @@ export const updateGateSelectMode = (mode : boolean) => async (dispatch : any) =
     }
 }
 
-export const addDroppedGate = (selectedGate : IGate) => async (dispatch : any) => {
+export const addDroppedGate = (gate : IGate) => async (dispatch : any) => {
     try {
-        dispatch({type: ADD_DROPPED_GATE_ACTION, payload: selectedGate})
+        console.log(`Adding dropped gate at cell [${gate.rowIndex}, ${gate.colIndex}]`);
+        dispatch({type: ADD_DROPPED_GATE_ACTION, payload: gate})
     }
     catch (e) {
         console.log("Error ", e);
         throw e;
     }
 }
+
+// export const removeDroppedGate = (gate : IGate) => async (dispatch : any) => {
+//     try {
+//         console.log(`Removing dropped gate at cell [${gate.rowIndex}, ${gate.colIndex}]`);
+//         dispatch({type: REMOVE_DROPPED_GATE_ACTION, payload: gate})
+//     }
+//     catch (e) {
+//         console.log("Error ", e);
+//         throw e;
+//     }
+// }
+
+export const removeDroppedGate = (id : string) => async (dispatch : any) => {
+    try {
+        console.log(`Removing dropped gate with id=${id}`);
+        dispatch({type: REMOVE_DROPPED_GATE_ACTION, payload: id})
+    }
+    catch (e) {
+        console.log("Error ", e);
+        throw e;
+    }
+}
+
+
+export const updateDraggingGate = (gate : IGate) => async (dispatch : any) => {
+    try {
+        dispatch({type: UPDATE_DRAGGING_GATE_ACTION, payload: gate})
+    }
+    catch (e) {
+        console.log("Error ", e);
+        throw e;
+    }
+}
+
+export const updateDraggingGatePosition = (x: number, y :number) => async (dispatch: any) =>{
+    try {
+        dispatch({type: UPDATE_DRAGGING_GATE_POSITION, payload: {x, y}})
+    }
+    catch (e) {
+        console.log("Error ", e);
+        throw e;
+    }
+}
+
+
+export const removeDraggingGate = () => async (dispatch : any) => {
+    try {
+        dispatch({type: REMOVE_DRAGGING_GATE_ACTION, payload: null})
+    }
+    catch (e) {
+        console.log("Error ", e);
+        throw e;
+    }
+}
+
+
--- src/redux/reducers/circuitConfigReducer.ts
+++ src/redux/reducers/circuitConfigReducer.ts
@@ -1,32 +1,35 @@
 import {Payload} from "../../common/types";
 import {
     ADD_DROPPED_GATE_ACTION,
-    INIT_SELECTED_STANDARD_GATE_ACTION, UPDATE_DEFAULT_STANDARD_GATE_ACTION,
+    INIT_SELECTED_STANDARD_GATE_ACTION, REMOVE_DRAGGING_GATE_ACTION,
+    REMOVE_DROPPED_GATE_ACTION,
+    UPDATE_DEFAULT_STANDARD_GATE_ACTION,
+    UPDATE_DRAGGING_GATE_ACTION, UPDATE_DRAGGING_GATE_POSITION,
     UPDATE_GATE_SELECT_MODE_ACTION,
     UPDATE_SELECTED_STANDARD_GATE_ACTION
+
 } from "../actions/circuitConfigAction";
 import {IGate} from "../../common/interfaces";
 import {Gate} from "../../common/classes";
 
-
-
 export interface CircuitConfigState {
     selectedStandardGate: string,
     gateSelectMode : boolean,
-    droppedGates : IGate[]
+    droppedGates : IGate[],
+    draggingGate : IGate
 }
 
 const initialCircuitConfigState = {
     selectedStandardGate : 'Standard Gate',
     gateSelectMode : false,
-    droppedGates : [new Gate(0, 39, 40, 38, 'X')]
+    droppedGates : [{} as Gate],
+    draggingGate : {} as Gate
 }
 
 function circuitConfigReducer (state = initialCircuitConfigState,
 action: Payload) {
     switch (action.type) {
         //Selected Gates Store Operations
-
         case INIT_SELECTED_STANDARD_GATE_ACTION:
             return {
                 ...state,
@@ -54,10 +57,43 @@ action: Payload) {
                 ...state,
                 droppedGates: [...state.droppedGates, action.payload]
             }
+        case REMOVE_DROPPED_GATE_ACTION:
+            return {
+                ...state,
+                droppedGates: state.droppedGates.filter(gate => (gate.id !== action.payload))
+            }
+
+        case UPDATE_DRAGGING_GATE_ACTION:
+            return {
+                ...state,
+                draggingGate : action.payload
+            }
+
+        case UPDATE_DRAGGING_GATE_POSITION:
+            return {
+                ...state,
+               draggingGate : {
+                   x: action.payload.x,
+                   y: action.payload.y,
+                   width: state.draggingGate.width,
+                   height: state.draggingGate.height,
+                   rowIndex : state.draggingGate.rowIndex,
+                   colIndex : state.draggingGate.colIndex,
+                   type: state.draggingGate.type
+                }
+            }
+
+        case REMOVE_DRAGGING_GATE_ACTION:
+            return {
+                ...state,
+                draggingGate: {} as Gate
+
+            }
 
         default:
             return state;
     }
 }
+
 
 export default circuitConfigReducer;
