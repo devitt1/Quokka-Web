b045c55b337a4fcaaa6eb2e669745fc01a829c35
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-01-27 16:37:47 +1100

    updated use gate id as key and remove mock data

--- src/components/Gate/DroppedGates/DroppedGates.tsx
+++ src/components/Gate/DroppedGates/DroppedGates.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styles from './DroppedGates.module.scss';
-import Gate from "../Gate";
 import {GateType} from "../../../common/types";
-import circuitConfigReducer from "../../../redux/reducers/circuitConfigReducer";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../redux/reducers/rootReducer";
+import DroppedGate from "./DroppedGate/DroppedGate";
 
 interface DroppedGatesProps {
 
@@ -15,22 +14,13 @@ const DroppedGates : React.FC<DroppedGatesProps> = () => {
     const {droppedGates} = useSelector((state : RootState) => (state.circuitConfig));
 
 
-    const gates = [
-        {
-            x: 48, y: 0, width:40, height:38, type: 'Y'
-        },
-        {
-            x: 96, y:39, width: 40, height: 38, type: 'Z'
-        },
-
-
-    ];
-
-    return (<g className={styles.droppedGate}>
+    return (<g className={styles.droppedGates}>
 
         {
             droppedGates.map((gate) => {
-                return <Gate x={gate.x} y={gate.y} width={40} height={38} type={gate.type as GateType}/>
+                return <DroppedGate key={gate.id} id={gate.id} x={gate.x} y={gate.y} rowIndex={gate.rowIndex}
+                                    colIndex={gate.colIndex} width={40} height={38}
+                                    type={gate.type as GateType}/>
             })
         }
 
