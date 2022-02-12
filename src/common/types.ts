eeec38899fee39bb6ff58596f03ba74ce2d9362a
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-02-13 10:35:09 +1100

    update: changed type definitions

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,5 +1,5 @@
 export type Payload = { type: string, payload: any};
-export const ALL_STD_GATES = ['X', 'Y', 'Z', 'RX', 'RY', 'RZ', 'C'];
+export const ALL_STD_GATES = ['X', 'Y', 'Z', 'RX', 'RY', 'RZ', 'CNOT'];
 export type StandardGateTypes = typeof ALL_STD_GATES;
 export type GateTypes = StandardGateTypes[number];
 export type ModalType = 'LoginModal' | 'ConnectionModal' | 'RunCircuitModal';
