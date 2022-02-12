1c91d0dc979614ac989641a5e9d2aa6ff27ea7ce
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-02-13 10:32:58 +1100

    update: added dimensions constants for standard gates, gate input, grid, add qubit button, gate extension

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,13 +1,41 @@
-const DIMENSION = {
+export const DIMENSIONS = {
     STD_GATE : {
-        width : 48,
-        height : 38
+        WIDTH : 40,
+        HEIGHT : 38
     },
     GATE_INPUT : {
-        width : 48,
-        height : 10
+        MARGIN: {
+            TOP : 5,
+            LEFT: 12,
+        },
+        PADDING: {
+            TOP: 2,
+            LEFT: 5,
+        },
+        WIDTH : 32,
+        HEIGHT : 15,
+        TEXT : {
+            MARGIN: {
+                TOP: 8
+            }
+        }
+    },
+    GRID : {
+        WIDTH: 48,
+        HEIGHT: 64,
+        PADDING : {
+            TOP: 21,
+        }
+    },
+    ADD_QUBIT_BTN : {
+        X_OFFSET: 9,
+        Y_OFFSET: 7
+    },
+    GATE_EXTENSION : {
+        WIDTH : 4
     }
 }
+
 
 export const ROUTES = {
     ABOUT : "/about",
