4ecf6787ddc2f54bf85a7038ec798076bef53f5b
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-02-28 20:52:17 +1100

    put to QASM to helpers functions

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,4 +1,4 @@
-import {IQubit} from "./interfaces";
+import {IGate, IQubit} from "./interfaces";
 
 export const sleep = (ms : number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -9,4 +9,17 @@ export const findQubitIndex = (id: string, qubits : IQubit[]) => {
         return qubit.id === id;
     });
     return index;
+}
+
+export const gateToQASM = (gate : IGate) => {
+    var qasmGateScript : string;
+    if (gate.type === 'CNOT') {
+        qasmGateScript = `cx`;
+    } else if (gate.rotAngle === null) {
+        qasmGateScript = `${gate.type.toLowerCase()}`;
+    } else {
+        qasmGateScript = `${gate.type.toLowerCase()}(${gate.rotAngle})`;
+    }
+    console.log("qasmGateScript=", qasmGateScript)
+    return qasmGateScript;
 }
