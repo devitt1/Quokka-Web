065dbaaef64ad8eeb7253bc454a65954a356c055
Peter Ho <ThienPhuc.Ho@uts.edu.au>
2022-02-28 20:53:26 +1100

    updated button components and stylings, component accept props that changes stylings

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import styles from './Button.module.scss';
+import {ButtonTypes} from "../../common/types";
 
 interface ButtonProps {
     name : string;
-    buttonStyle : string;
+    types : ButtonTypes[];
     onClick : any;
-    selected : boolean
+    selected? : boolean;
+    leftImageSource? : any;
+    rightImageSource? : any;
 }
 
+
 export const Button : React.FC<ButtonProps> = (props) => {
-    var buttonStyle = styles.button;
-    if (props.buttonStyle === "selectGateBtn") {
-        buttonStyle += ' ' + styles.selectGateBtn;
-        if (props.selected) {
-            buttonStyle += ' ' + styles.selected;
-        }
-    } else if (props.buttonStyle === "runCircuitBtn") {
-        buttonStyle = styles.button + ' ' + styles.runCircuitBtn;
+    const {types, selected, onClick, name, leftImageSource, rightImageSource, children} = props;
+    const buttonStyle = [styles.button];
+
+    types.forEach((type : ButtonTypes) => {
+        buttonStyle.push(styles[type]);
+    })
+
+    if (selected) {
+        buttonStyle.push(styles['selected']);
     }
 
     return (<div className={styles.button}>
-        <button className={buttonStyle} onClick={props.onClick}>
-            {props.name}
+        <button className={buttonStyle.join(' ')} onClick={onClick}>
+            {
+                leftImageSource ?
+                    <img className={styles.leftImage} src={leftImageSource}/>
+                    : null
+            }
+            <div className={styles.btnLbl}>{name}</div>
+            {
+                rightImageSource ?
+                    <img className={styles.rightImage}  src={rightImageSource}/>
+                    : null
+            }
         </button>
-
     </div>)
 }
